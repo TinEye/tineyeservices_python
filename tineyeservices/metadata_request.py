e8779ee7ae6115cc05fe8343511ab97cc176ae2b
TinEye dev <support@tineye.com>
2013-03-15 11:24:52 -0400

    Updating copyright.

--- tineyeservices/metadata_request.py
+++ tineyeservices/metadata_request.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2012 Idee Inc. All rights reserved worldwide.
+# Copyright (c) 2012-2013 Idee Inc. All rights reserved worldwide.
 
 from image import Image
 from tineye_service_request import TinEyeServiceRequest
