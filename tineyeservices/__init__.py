60e52fc3c42d76d09794492d12e425956eff3da4
TinEye dev <support@tineye.com>
2016-10-25 14:50:47 -0400

    Added WineEngineRequest subclass.

--- tineyeservices/__init__.py
+++ tineyeservices/__init__.py
@@ -1,7 +1,8 @@
-# Copyright (c) 2012-2013 Idee Inc. All rights reserved worldwide.
+# Copyright (c) 2012-2016 Idee Inc. All rights reserved worldwide.
 
 from exception import *
 from image import Image
 from matchengine_request import MatchEngineRequest
 from mobileengine_request import MobileEngineRequest
 from multicolorengine_request import MulticolorEngineRequest
+from wineengine_request import WineEngineRequest
---
+++ tineyeservices/wineengine_request.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016 Idée Inc. All rights reserved worldwide.
+
+from mobileengine_request import MobileEngineRequest
+
+
+class WineEngineRequest(MobileEngineRequest):
+    """
+    Class to send requests to a WineEngine API.
+
+    Adding an image using data:
+
+        >>> from tineyeservices import WineEngineRequest, Image
+        >>> api = WineEngineRequest(api_url='http://localhost/rest/')
+        >>> image = Image(filepath='/path/to/image.jpg')
+        >>> api.add_image(images=[image])
+        {u'error': [], u'method': u'add', u'result': [], u'status': u'ok'}
+
+    Searching for an image using an image URL:
+
+        >>> api.search_url(url='http://www.tineye.com/images/meloncat.jpg')
+        {'error': [],
+         'method': 'search',
+         'result': [{'filepath': 'match1.png',
+                     'score': '97.2',
+                     'overlay': 'overlay/query.png/match1.png[...]'}],
+         'status': 'ok'}
+    """
+
+    def __repr__(self):
+        return "WineEngineRequest(api_url=%r, username=%r, password=%r)" %\
+               (self.api_url, self.username, self.password)
